056e809b4773ce0be945a9a788de4499fa0a727a
Aurélien Perrier <a.perrier89@gmail.com>
2018-05-14 15:52:13 +0200

    Fixing search

--- app/controllers/search/computers/find.js
+++ app/controllers/search/computers/find.js
@@ -15,39 +15,39 @@ module.exports = {
         // local table, search
         if (req.query.model != null && validator.isLength(req.query.model, { min: 0, max: 40 }))
             conditions["model"] = { $like: '%' + req.query.model + '%' };
-
-        // ////////////////////////
-        // // activity
-        // if (req.query.activity != null && validator.isLength(req.query.activity, { min: 0, max: 40 }))
-        //     includes.push({ model: Models.Computer.MComputer_activity, as: 'activity', attributes: { exclude: 'id' },  where: { name: req.query.activity } })
-        // else
-        //     includes.push({ model: Models.Computer.MComputer_activity, as: 'activity', attributes: { exclude: 'id' } })
-
-        // // OS
-        // if (req.query.os != null && validator.isLength(req.query.os, { min: 0, max: 40 }))
-        //     includes.push({ model: Models.Computer.MComputer_os, as: 'os', attributes: { exclude: 'id' },  where: { name: req.query.os } })
-        // else
-        //     includes.push({ model: Models.Computer.MComputer_os, as: 'os', attributes: { exclude: 'id' } })
         
-        // // Keyboard
-        // if (req.query.keyboard != null && validator.isLength(req.query.keyboard, { min: 0, max: 40 }))
-        //     includes.push({ model: Models.Computer.MComputer_keyboard, as: 'keyboards', attributes: { exclude: 'id' },  where: { type: req.query.keyboard } })
-        // else
-        //     includes.push({ model: Models.Computer.MComputer_keyboard, as: 'keyboards', attributes: { exclude: 'id' } })
+        // OS
+        if (req.query.os != null && req.query.os != "")
+            conditions["osid"] = { $eq: req.query.os };
+
+        // activity
+        if (req.query.activity != null && req.query.activity != "")
+            conditions["activityid"] = { $eq: req.query.activity };
+
+        // Keyboard
+        if (req.query.keyboard != null && req.query.keyboard != "")
+            conditions["keyboardid"] = { $eq: req.query.keyboard };
             
-        // // Graphics
-        // if (req.query.graphics != null && validator.isLength(req.query.graphics, { min: 0, max: 40 }))
-        //     includes.push({ model: Models.Computer.MComputer_graphic, as: 'graphics', attributes: { exclude: 'id' },  where: { model: req.query.graphics } })
-        // else
-        //     includes.push({ model: Models.Computer.MComputer_graphic, as: 'graphics', attributes: { exclude: 'id' } })
+        // Graphics
+        if (req.query.graphics != null && req.query.graphics != "")
+            conditions["gpuid"] = { $eq: req.query.gpu };
+            
+        // Chipsets
+        if (req.query.chipset != null && req.query.chipset != "")
+            conditions["chipsetid"] = { $eq: req.query.chipset };
+
+        // Chipsets
+        if (req.query.screen != null && req.query.screen != "")
+            conditions["screenid"] = { $eq: req.query.screen };
 
         // Push all relation, search no avalailble
         includes.push(
-            { model: Models["computers_os"], attributes: { exclude: ['id', 'description'] } },
-            { model: Models["computers_cpus"], attributes: { exclude: ['id', 'description'] } },
-            { model: Models["computers_gpus"], attributes: { exclude: ['id', 'description'] } },
-            { model: Models["computers_chipsets"], attributes: { exclude: ['id', 'description'] } },
-            { model: Models["computers_activities"], attributes: { exclude: ['id', 'description'] } },
+            { model: Models["computers_os"], attributes: { exclude: ['id'] } },
+            { model: Models["computers_cpus"], attributes: { exclude: ['id'] } },
+            { model: Models["computers_gpus"], attributes: { exclude: ['id'] } },
+            { model: Models["computers_chipsets"], attributes: { exclude: ['id'] } },
+            { model: Models["computers_activities"], attributes: { exclude: ['id'] } },
+            { model: Models["computers_screens"], attributes: { exclude: ['id'] } },
         )
         
         // request
