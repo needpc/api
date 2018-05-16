71bf2e97d4ae17bca3b80707cb17f6b1d6f89f16
Aurélien Perrier <a.perrier89@gmail.com>
2018-05-16 10:33:05 +0200

    Updating question controller

--- app/controllers/search/questions/find.js
+++ app/controllers/search/questions/find.js
@@ -3,30 +3,6 @@ var validator = require('validator');
 var path = require('path');
 var Models   = require(path.join(__dirname, '../../../models/index'));
 
-var taleQuestionAnsewr = [];
-
-// show only important informations
-var setTable = function(itemid) {
-    var tableAnsewOrder = [];
-    var tableAnsewIndice = [];
-
-    // write info in new array
-    for (var i = 0; i < itemid.quests.length; i++) {
-        tableAnsewOrder.push(itemid.quests[i].resp);
-        tableAnsewIndice.push(itemid.quests[i].indice);
-    }
-    
-    // Push all information
-    taleQuestionAnsewr.push({
-        "id": itemid.id,
-        "activity": itemid.activity,
-        "question": itemid.quest,
-        "domain" : itemid.domain,
-        "reponses" : tableAnsewOrder,
-        "indice" : tableAnsewIndice
-    });
-};
-
 module.exports = {
 
     // Get all question (general || domain)
@@ -34,25 +10,26 @@ module.exports = {
 
         // Include relations
         includes = [
-            { model: Models["computers_quests_resps"], as: 'quests', attributes: { exclude: ['id', 'createdat', 'updatedat', 'quest_id'] } }
+            { model: Models["computers_quests_resps"], attributes: { exclude: ['id', 'questid', 'createdAt', 'updatedAt'] } }
         ]
 
         // query activity
-        if (req.query.activity != null && validator.isLength(req.query.activity, { min: 0, max: 40 }))
-            includes.push({ model: Models["computers_activities"], as: 'activity', attributes: { exclude: 'id' },  where: { name: req.query.activity } })
-        else
-            includes.push({ model: Models["computers_activities"], as: 'activity', attributes: { exclude: 'id' } })
+        // if (req.query.activity != null && validator.isLength(req.query.activity, { min: 0, max: 40 }))
+        //     includes.push({ model: Models["computers_activities"], as: 'activity', attributes: { exclude: 'id' },  where: { name: req.query.activity } })
+        // else
+        //     includes.push({ model: Models["computers_activities"], as: 'activity', attributes: { exclude: 'id' } })
 
         // search in database
         Models["computers_quests"].findAll({
             include: includes,
-            attributes: { exclude: ['createdat', 'updatedat'] }
+            attributes: { 
+                exclude: [
+                    'createdAt', 
+                    'updatedAt'
+                ] 
+            }
         }).then(function (question) {
-            question.forEach(function(element) {
-                setTable(element);
-            });
-            error.http_success(req, res, { code: 200, data: taleQuestionAnsewr });
-            taleQuestionAnsewr = [];
+            error.http_success(req, res, { code: 200, data: question });
         });
     }
 };
